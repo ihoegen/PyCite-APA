9a6c5f8131e633840705dca3557f6a98080f731f
Ian Hoegen <ianhoegen@gmail.com>
2015-06-27 23:41:24 -0700

    switched elifs into join() functions, changed from write file to append

--- PyCiteAPAFree.py
+++ PyCiteAPAFree.py
@@ -1,45 +1,53 @@
 #Created 2015 By Ian Hoegen, Hoegen Developments
-#Picks what type of work is being cited
 def apa():
-    type_of_document = raw_input('Are you citing a website, a newspaper, a book, or a journal? ')
+    #Picks what type of work is being cited
+    document_type = raw_input('Are you citing a website, a newspaper, a book,'\
+                              ' or a journal? ')
     print""
-    type_of_document = type_of_document.lower()
-    if type_of_document == 'website':
-        apa_web()
-    elif type_of_document == 'newspaper':
-        apa_news()
-    elif type_of_document == 'book':
-        apa_book()
-    elif type_of_document == 'journal':
-        apa_journal()
+    document_type = document_type.lower()
+    if document_type == 'website':
+        website()
+    elif document_type == 'newspaper':
+        newspaper()
+    elif document_type == 'book':
+        book()
+    elif document_type == 'journal':
+        journal()
     else:
         print "Please choose a website, a newspaper, a book, or a journal"
         apa()
-#helper function to print
+             
+
 def print_m(citation):
+    #helper function to print to file and to console
     print "Your citation is: "
     print ""
     print citation
-    f = open('references.odt', 'w+')
-    f.write(citation)
+    f = open('references.odt', 'a')
+    f.write(citation + '\n')
     f.write('')
-#Defines the year
+    
 def year():
-    year = raw_input("What year was this published, followed by the month and date. Enter n.d if unknown ")
+    #Defines the year of the work
+    year = raw_input("What year was this published, followed by the month and"\
+                     "date. Enter n.d if unknown ")
     print""
-    return  "(%s)" % (year)
-#Helper function for title
-def apa_title():
+    return  "(%s) " % (year)
+
+def title():
+    #Recieves and formats the title
     title = raw_input("What is the title of the work? ")
     print""
     title_first = title[0].upper()
     title_rest = title[1:].lower()
     title = title_first + title_rest
     return  "%s. " % (title)
-#Compiles the author info
-def apa_author():
+
+def info():
+    #Recieves and formats the author info, and joins it with the year and title
     author_list = []
     x = 1
+    
     amount_authors = raw_input("How many authors are there? Select 1-4. ")
     print''
     amount_authors = int(amount_authors)
@@ -54,31 +62,27 @@ def apa_author():
             author_full = "%s, %s. " % (author_last, author_first)
             author_list.append(author_full)
             x += 1
-        if n == 1:
-            author_cite = str(author_list[0])
-        elif n == 2:
-            author_cite = str(author_list[0]) + str(author_list[1])
-        elif n  == 3:
-            author_cite = str(author_list[0]) + str(author_list[1]) + str(author_list [2])
-        else:
-            author_cite = str(author_list[0]) + str(author_list[1]) + str(author_list[2]) + str(author_list[3])
+        author_cite = "".join(author_list)
     else:
         print "You need to enter a number between 1 and 4"
         print ''
-        apa_author()
-    return author_cite + apa_title() + year()
-#Website citations
-def apa_web():
+        info()
+    return author_cite + title() + year()
+
+def website():
+    #Website citations
     retrieved_from = raw_input("What is the address of the website? ")
     print''
     date = raw_input("What day did you visit this site? ")
     print''
     retrieved_from = "Retrieved %s from %s" % (date, retrieved_from)
-    citation = apa_author() + retrieved_from
+    
+    citation = info() + retrieved_from
     print_m(citation)
-    copy()
-#News Citations
-def apa_news():
+    exit_messages()
+
+def newspaper():
+    #Newspaper Citations
     publisher = raw_input("What is the name of the publishing company? ")
     print''
     page = raw_input("What page(s) did you find the article on? ")
@@ -87,6 +91,7 @@ def apa_news():
     electronic = raw_input("Was this retrieved from a website? y/n ")
     print ''
     electronic = electronic.lower()
+    
     if electronic == 'y':
         site = raw_input('What is the website you found it on? ')
         print ''
@@ -95,11 +100,13 @@ def apa_news():
         site = "Retrieved %s from %s" % (date, site)
     else:
         site = " "
-    citation = apa_author() + publisher + site
+    
+    citation = info() + publisher + site
     print_m(citation)
-    copy()
-#Book citations
-def apa_book():
+    exit_messages()
+
+def book():
+    #Book citations
     location = raw_input('Where was the book published? ')
     print''
     location = location[0].upper() + location[1:].lower()
@@ -108,11 +115,12 @@ def apa_book():
     print''
     publisher = publisher[0].upper() + publisher[1:].lower()
     publisher = "%s." % (publisher)
-    citation = apa_author() + location + publisher
+    citation = info() + location + publisher
     print_m(citation)
-    copy()
-#Journal Citation
-def apa_journal():
+    exit_messages()
+
+def journal():
+    #Journal Citation
     journal_name = raw_input('What is the name of the journal? ')
     journal_name = journal_name[0].upper() + journal_name[1]
     print ''
@@ -124,22 +132,27 @@ def apa_journal():
     print ''
     web = raw_input('Was it found on a website? y/n ')
     web = web.lower()
+    
     if web == "y":
         site = raw_input('What website was it found on? ')
         date = raw_input("What day did you visit it? ")
         web_and_date = " Retrieved from: %s on %s " % (site, date)
     else:
         web_and_date = " "
+        
     journal_name = " %s, %s, %s. doi: %s." % (journal_name, volume, pages, doi)
-    citation = apa_author() + journal_name + web_and_date
+    citation = info() + journal_name + web_and_date
     print_m(citation)
-    copy()
-#Welcome Messages
-def copy():
+    exit_messages()
+
+def exit_messages():
+    #messages displayed after the citation
     print ""
     print "Your file can be found in the document references.odt"
     print""
-    print "To copy  from here, right click the title bar, highlight over the edit item and select mark. Highlight the citation and hit enter on your keyboard. Your citation is now ready for use"
+    print "To copy  from here, right click the title bar, highlight over the"\
+          "edit item and select mark. Highlight the citation and hit enter on"\
+          "your keyboard. Your citation is now ready for use"
     print ""
     more = raw_input('Would you like to PyCite another item? y/n ')
     if more.lower() == "y":
@@ -147,5 +160,6 @@ def copy():
     else:
         print "Goodbye"
         exit()
-print "Welcome to PyCite APA Free Edition, for MLA and CMS be sure to download the full version."
+        
+print "Welcome to PyCite APA Free Edition"
 apa()
